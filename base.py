96079a8003f3599dd2e994a965e90f8d1cd097d2
Ananya <Ananya123>
2020-05-13 21:15:41 +0200

    removed unnecessary db populating in the beginning

--- base.py
+++ base.py
@@ -9,22 +9,25 @@ file_extensions = ('.jpg', '.png')
 timer = 1
 ################################################################################
 
-# If no files in db
-if is_table_empty():
-    # Store all possible files with status "not_viewed"
-    files_matched = list_all_pictures(file_path, file_extensions)
-    populate_db_with_paths(files_matched)
-    files_not_viewed = files_matched
-else:
-    files_not_viewed = list_not_viewed()
+
+# files_matched = list_all_pictures(file_path, file_extensions)
+# populate_db_with_paths(files_matched)
+
+
+files_matched = list_all_pictures(file_path, file_extensions)
+files_not_viewed = files_matched
 
 # Choose one and change its status from  "not_viewed" to "viewed" in db
-while len(files_not_viewed) != 0:
-    chosen = random.choice(files_not_viewed)
-    pictureObj = Picture(chosen)
-    # Display the chosen for <timer> seconds
-    pictureObj.display(timer)
-    # Change the status of "chosen" to "viewed"
-    mark_as_viewed(chosen)
+while len(files_matched) != 0:
+    chosen = random.choice(files_matched)
+    if is_not_viewed(chosen):
+        pictureObj = Picture(chosen)
+        # Display the chosen for <timer> seconds
+        pictureObj.display(timer)
+        # Change the status of "chosen" to "viewed"
+        mark_as_viewed(chosen)
+    else :
+        # skip to next
+        pass
     # Remove chosen ones from variable to avoid reead from db
     files_not_viewed = list(set(files_not_viewed) - set([chosen]))
--- db_operations.py
+++ db_operations.py
@@ -1,6 +1,8 @@
 from models import *
 import datetime
 
+not_viewed = "not_viewed"
+viewed = "viewed"
 
 def is_table_empty():
     picture_list_query = Image.select()
@@ -17,7 +19,7 @@ def list_viewed():
 
 
 def list_not_viewed():
-    pictures_not_viewed = list(Image.select().where(Image.status=="not_viewed").execute())
+    pictures_not_viewed = list(Image.select().where(Image.status==not_viewed).execute())
     file_paths = []
     for item in pictures_not_viewed:
         file_path = item.filepath
@@ -27,7 +29,7 @@ def list_not_viewed():
 
 def mark_as_viewed(filepath):
     image_record = Image.get(Image.filepath==filepath)
-    image_record.status = "viewed"
+    image_record.status = viewed
     image_record.save()
     image_record.viewed_date = datetime.datetime.now()
     image_record.save()
@@ -44,3 +46,20 @@ def mark_as_not_viewed():
 def populate_db(file_path, status):
     picture_entry = Image(filepath=file_path, status=status, viewed_date=None)
     picture_entry.save()
+
+
+def is_not_viewed(filepath):
+    # Optimization needed here
+    image_record_query = Image.select().where(Image.filepath == filepath)
+    if image_record_query.exists():
+        image_status = Image.get(Image.filepath == filepath).status
+        if image_status==not_viewed:
+            return True
+        else:
+            return False
+    else :
+        status = not_viewed
+        image_entry = Image(filepath=filepath, status=status,
+                              viewed_date=None)
+        image_entry.save()
+        return True
